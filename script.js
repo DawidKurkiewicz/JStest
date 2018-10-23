0b83d3bf1ca7c19917c30e70a89914976c7f5846
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-10-23 14:05:38 +0200

    arrays

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-var numbers = [] ;
-for ( var i = 0; i < 10; i++){
-    numbers[i] =(Math.random() * 10);
-}
-for(var i = 0; i < numbers.length; i++) {
-    if (numbers[i] > 5){
-        break
-    }
-console.log(numbers [i]);
+var array = [false, 0, true, 'ala', null];
+console.log(array[2]);
+
+var newArray = array;
+
+//---------
+
+var obj = {
+    ala: false,
+    length: 123,
+    elements: ['ola', false, null, 'mariusz'],
+    'index-1': 'pozycja-1',
+    'index-2': 'pozycja-2',
+    'index-3': 'pozycja-3',
+};
+
+console.log(obj.ala);
+console.log(obj.length);
+console.log(obj.elements[3]);
+
+for (let i = 0; i < 3; i++){
+    console.log(obj['index-' +(i + 1)]);
 }
