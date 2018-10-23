125ef0bc323f49e368dc1c742d2050f4bf4e3666
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-10-23 15:07:22 +0200

    slice exercise

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-var array = ['one', 'two', 'three', 'four', 'five', 'six']
+/*ar array = ['one', 'two', 'three', 'four', 'five', 'six']
 var b = array.slice(2, 5);
 console.log(array);
 console.log(b);
 var c = array.slice(0, 2) .concat (array.slice (3, 6));
 console.log(c)
+var d = array.slice(-4, 5);
+console.log(d);*/
+
+var numbers = [];
+var numbersSize = 14;
+for (var i = 0; i < numbersSize; i++) {
+  numbers[i] = parseInt(Math.random() * 10 + 1);
+}
+
+console.log('tablica poczatkowa = ', numbers);
+
+var startIndex = (numbersSize / 2) - 2;
+console.log('startIndex = ', startIndex);
+var numberOfItems = 4;
+var newArray = numbers.slice(startIndex, startIndex + numberOfItems);
+console.log('srodkowe 4 elementy = ', newArray);
