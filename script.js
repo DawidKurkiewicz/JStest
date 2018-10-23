b7b68b81e42493e02dfd915f14e64c2ffb1fd338
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-10-23 14:30:30 +0200

    pop, push, shift, unshift

--- script.js
+++ script.js
@@ -1,23 +1,8 @@
-var array = [false, 0, true, 'ala', null];
-console.log(array[2]);
+var a = [1, 2, 3, 4, 5];
 
-var newArray = array;
-
-//---------
-
-var obj = {
-    ala: false,
-    length: 123,
-    elements: ['ola', false, null, 'mariusz'],
-    'index-1': 'pozycja-1',
-    'index-2': 'pozycja-2',
-    'index-3': 'pozycja-3',
-};
-
-console.log(obj.ala);
-console.log(obj.length);
-console.log(obj.elements[3]);
-
-for (let i = 0; i < 3; i++){
-    console.log(obj['index-' +(i + 1)]);
-}
+a.unshift(0);
+a.push(6);
+console.log(a);
+a.shift();
+a.pop();
+console.log(a);
