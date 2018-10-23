56659dab82e14d8c25392dbac1fc9f94030fbb2d
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-10-23 09:56:03 +0200

    logic operators

--- script.js
+++ script.js
@@ -1,23 +1,4 @@
-var obj1 = {ala: 'ala ma psa'}
-var obj2 = {ola: 'ola ma psa'}
-var obj3 = {ela: 'ela ma psa'}
-var obj4 = {ada: 'ada ma psa'}
-var obj5 = {ida: 'ida ma psa'}
-var obj6 = {max: 'max ma psa'}
-var obj7 = {jan: 'jan ma psa'}
-var obj8 = {jon: 'jon ma psa'}
-var obj9 = {mit: 'mit ma psa'}
-var obj10 = {kil: 'kil ma psa'}
+var a = 3
+var b = 2
 
-obj1 = obj2
-
-obj3 = obj5
-
-obj4 = obj8
-
-
-obj4 == obj10
-
-
-console.log(obj4);
-
+console.log( a && b);
