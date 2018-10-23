32ecb1db5519800715017a628fb7cbba6bfcd6ed
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-10-23 12:10:56 +0200

    math random

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-var numbers = [1, 2, 3, 4, 5]
 
+var numbers = [];
+for (var i = 0; i < 1000; i++){
+    numbers[i] = Math.random();
+
+}
 console.time('in loop')
 for (var i = 0; i < numbers.length; i++){
     console.log(numbers[i])
@@ -8,7 +12,7 @@ console.timeEnd('in loop')
 
 console.time('before loop')
 var length = numbers.length
-for (var i = 0; i <length; i++) {
+for (var i = 0; i < length; i++) {
     console.log(numbers[i])
 }
 console.timeEnd('before loop')
