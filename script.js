cfb29b58d63336efa3c951410254f9b32d262fd5
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-10-24 10:21:57 +0200

    exercise

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 var myCar = {
   make: 'ford',
   model: 'mustang',
-  sound: function(){
+  sound: function() {
     console.log('wrrrrr!');
+  },
+  go: function() {
+    console.log('go go go');
+    return 'jestem w funckji go';
   }
 }
-
 myCar.sound();
+myCar.go();
+console.log(myCar.go());
+
