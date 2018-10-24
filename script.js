b5f23cdae439338b0b7e16c840c25d2ddd70278c
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-10-24 11:12:39 +0200

    const

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-function mul(a, b) {
-  return a * b;
-}
-function sum (a, b) {
-  return a + b;
-}
-var go = function(a, b) {
-  return a + b;
-}
-function isa(a, b, mulFunction) {
-  return (a + b) + mulFunction(a, b);
-}
+const kop = (a, b, c) =>  (a*b)/c
 
-var superSum = sum;
-var superSum2 = sum(5, 6);
-
-console.log(sum (1, 2));
-console.log(go(3, 4));
-console.log(superSum(3, 3));
-console.log(superSum2)
-console.log(isa(4, 5, mul));
+console.log(kop(3, 4, 5))
