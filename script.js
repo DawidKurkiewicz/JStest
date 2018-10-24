418bf4a1c2f4bc6065ba4c2680e6e37222f6f573
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-10-24 11:31:00 +0200

    bledy itd

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-const kop = (a, b, c) =>  (a*b)/c
 
-console.log(kop(3, 4, 5))
+
+
+function isa(a, b,) {
+  var d = a + b;
+  c = a + b
+  return d * 3;
+}
+
+function add (a, b) {
+  var sum = a + b;
+  return sum;
+}
+
+
+
+console.log(isa (2, 3))
+console.log(c);
+console.log(d); // celowy błąd
+console.log(add(1, 2)); //to się nie wykona
+console.log(sum); // błąd też
+console.log('hello');
