643801d1f8dcaa88aa14dba82a429c1b7363927d
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-10-24 14:50:58 +0200

    storage properties

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-function loop (x) {
-  console.log(x);
-  if (x >= 17) {
-    return;
-  
+var index =   sessionStorage.getItem('index') || 0 ;
+
+
+
+function addPerson (name) {
+  localStorage.setItem('person' + index, name);
+  index++;
+  sessionStorage.setItem('index', index);
+
+
 }
-loop(x+1);
+
+function removePerson (key) {
+  localStorage.removeItem(key)
 }
-console.log(loop(6))
+
+
+addPerson ('marek');
+addPerson ('da');
+addPerson ('wa');
+addPerson ('fa');
+addPerson ('xa');
+addPerson ('ds');
+
