4a2d73c84e3bfcaaf2f8f1f8a5bcc0667bc92d16
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-10-24 10:18:07 +0200

    function property

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
-var myCar = new Object
-
-myCar.mark = 'Peugeot';
-myCar.date = 2006;
-myCar.model = 307;
-myCar.km = '144k';
-
-console.log('my car model is ' + myCar.model)
-
-var myDog = {
-
-  race: 'shepherd',
-  date: 2013,
-  model: 'German',
+var myCar = {
+  make: 'ford',
+  model: 'mustang',
+  sound: function(){
+    console.log('wrrrrr!');
   }
-  console.log('my dog race is ' + myDog.model)
+}
 
-
-
-
-  console.log(Object.assign({}, myCar, myDog));
+myCar.sound();
