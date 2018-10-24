285a8fade08b4fabf9f8ef4029d5e91dc16d1b37
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-10-24 09:39:25 +0200

    index of array

--- script.js
+++ script.js
@@ -1,22 +1,7 @@
-/*ar array = ['one', 'two', 'three', 'four', 'five', 'six']
-var b = array.slice(2, 5);
-console.log(array);
-console.log(b);
-var c = array.slice(0, 2) .concat (array.slice (3, 6));
-console.log(c)
-var d = array.slice(-4, 5);
-console.log(d);*/
+var array = [1, 2 ,5, 6, 9, 5, 8]
 
-var numbers = [];
-var numbersSize = 14;
-for (var i = 0; i < numbersSize; i++) {
-  numbers[i] = parseInt(Math.random() * 10 + 1);
-}
+console.log(array.indexOf(5))
 
-console.log('tablica poczatkowa = ', numbers);
+var array2 = [ 'ala', 'ma', 'kota']
 
-var startIndex = (numbersSize / 2) - 2;
-console.log('startIndex = ', startIndex);
-var numberOfItems = 4;
-var newArray = numbers.slice(startIndex, startIndex + numberOfItems);
-console.log('srodkowe 4 elementy = ', newArray);
+console.log(array2.indexOf ('kota'));
