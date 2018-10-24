a2b9c0bac76808ea91d526fb8c8a9e3e7b7075dd
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-10-24 10:12:26 +0200

    object exercise

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-var myCar = {
+var myCar = new Object
 
-mark: 'Peugeot',
-date: 2006,
-model: 307,
-km: '144k',
-}
-console.log(myCar.mark)
+myCar.mark = 'Peugeot';
+myCar.date = 2006;
+myCar.model = 307;
+myCar.km = '144k';
+
+console.log('my car model is ' + myCar.model)
+
+var myDog = {
+
+  race: 'shepherd',
+  date: 2013,
+  model: 'German',
+  }
+  console.log('my dog race is ' + myDog.model)
+
+
+
+
+  console.log(Object.assign({}, myCar, myDog));
