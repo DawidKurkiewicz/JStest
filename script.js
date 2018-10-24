90c17881eb63072b4585d7951ac25e96cb9790f0
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-10-24 11:46:05 +0200

    strict

--- script.js
+++ script.js
@@ -1,22 +1,9 @@
+'use strict';
 
-
-
-function isa(a, b,) {
-  var d = a + b;
-  c = a + b
-  return d * 3;
-}
 
 function add (a, b) {
-  var sum = a + b;
+  sum = a + b;
   return sum;
 }
 
-
-
-console.log(isa (2, 3))
-console.log(c);
-console.log(d); // celowy błąd
-console.log(add(1, 2)); //to się nie wykona
-console.log(sum); // błąd też
-console.log('hello');
+console.log(add(1, 2), sum);
