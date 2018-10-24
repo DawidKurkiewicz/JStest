7a80ebd4c7a2ea1a5bac18b19119aebcf37d30b1
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-10-24 10:58:16 +0200

    more sum fun

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+function mul(a, b) {
+  return a * b;
+}
 function sum (a, b) {
   return a + b;
 }
-
-
 var go = function(a, b) {
   return a + b;
-
 }
+function isa(a, b, mulFunction) {
+  return (a + b) + mulFunction(a, b);
+}
+
 var superSum = sum;
 var superSum2 = sum(5, 6);
 
@@ -14,3 +18,4 @@ console.log(sum (1, 2));
 console.log(go(3, 4));
 console.log(superSum(3, 3));
 console.log(superSum2)
+console.log(isa(4, 5, mul));
