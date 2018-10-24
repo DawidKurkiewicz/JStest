ac378735dd8eabed82fdc29c6d90c0b3e23d6355
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-10-24 09:50:44 +0200

    exercise

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-var array = [1, 2 ,5, 6, 9, 5, 8]
+var array = [1, 2 ,3, 4, 5, 6, 7, 8, 9, 10]
 
-console.log(array.indexOf(5))
 
-var array2 = [ 'ala', 'ma', 'kota']
+var x = 7
 
-console.log(array2.indexOf ('kota'));
+for ( var i = 0; i < array.length; i++) {
+  if (array[i] == x) {
+    console.log(x)
+}
+}
+
+if (array.indexOf(x) !== -1) {
+  console.log(x);
+}
