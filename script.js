a8acdd1d41372391c5f9a0b0f4c830504a9166df
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-10-26 11:35:28 +0200

    exercise 1 + 2

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
+// document.querySelector('h1').style.color ='red' ;
+// document.querySelector('h1').style.fontSize ='80px' ;
+
+
+
+
 const akapity = document.querySelectorAll('p');
 let i;
 for (i = 0; i  < akapity.length; i++) {
     akapity[i].style.backgroundColor = 'yellow';
     akapity[i].style.color = 'blue';
     akapity[i].style.fontSize = '40px';
-    akapity[i].style.margin = '100px';
-
-  }
+}
 
 
-const list = document.querySelectorAll('h1:first-child');
+const list = document.querySelectorAll('h1');
 let index;
-for (index = 0; index < list.length; index++) {
+for (index = 0; index < list.length; ++index) {
     list[index].style.fontSize = "60px";
-    list[index].style.color = 'red';
+    list[index].style.color = 'red'
 
 }
 
 
-const lista = document.querySelectorAll('h1:nth-child(2)');
-let index1;
-for (index1 = 0; index1 < lista.length; index1++) {
-    lista[index1].style.fontSize = "60px";
-    lista[index1].style.color = 'purple';
+
+
+
+
+function pozycjaKursora(e) {
+  alert ('Pozycja kursora na ekranie: ' + e.screenX + ', ' + e.screenY + ", czas " + e.timeStamp + ', zdarzenie ' + event.type);
 }
+window.addEventListener('click', pozycjaKursora);
