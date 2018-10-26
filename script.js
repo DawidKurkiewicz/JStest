23c660b81b48ddad0ed06e37b7791156c0dfe34e
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-10-26 10:53:30 +0200

    exercise 1 more

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-// document.querySelector('h1').style.color ='red' ;
-// document.querySelector('h1').style.fontSize ='80px' ;
-
-
-
-
 const akapity = document.querySelectorAll('p');
 let i;
 for (i = 0; i  < akapity.length; i++) {
@@ -13,9 +7,17 @@ for (i = 0; i  < akapity.length; i++) {
 }
 
 
-const list = document.querySelectorAll('h1');
+const list = document.querySelectorAll('h1:first-child');
 let index;
-for (index = 0; index < list.length; ++index) {
+for (index = 0; index < list.length; index++) {
     list[index].style.fontSize = "60px";
-    list[index].style.color = 'red'
+    list[index].style.color = 'red';
+}
+
+
+const lista = document.querySelectorAll('h1:nth-child(2)');
+let index1;
+for (index1 = 0; index1 < lista.length; index1++) {
+    lista[index1].style.fontSize = "60px";
+    lista[index1].style.color = 'purple';
 }
