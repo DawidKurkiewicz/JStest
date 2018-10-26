409820f583038dd227d829f1d466556e9e40bc9b
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-10-26 13:32:22 +0200

    exercise 3 modifacation

--- script.js
+++ script.js
@@ -1,47 +1,8 @@
-// document.querySelector('h1').style.color ='red' ;
-// document.querySelector('h1').style.fontSize ='80px' ;
-
-
-
-const list = document.querySelectorAll('h1');
-let index;
-for (index = 0; index < list.length; ++index) {
-    list[index].style.fontSize = "60px";
-    list[index].style.color = 'red'
-    list[index].addEventListener('click', color1);
-
-
-
-}
-const akapity = document.querySelectorAll('p');
-let i;
-for (i = 0; i  < akapity.length; i++) {
-    akapity[i].style.backgroundColor = 'yellow';
-    akapity[i].style.color = 'blue';
-    akapity[i].style.fontSize = '40px';
-    akapity[i].addEventListener('click', color);
-    akapity[i].addEventListener('dblclick', color1);
-
-   
-
-
-}
-function color() {
-  this.style.backgroundColor = 'green'
-  this.style.fontSize = '80px'
-  this.style.color = 'black'
-
-
-}
-function color1() {
-  this.style.backgroundColor = 'yellow'
-  this.style.fontSize = '40px'
-  this.style.color = 'blue'
-
-}
-// function pozycjaKursora(e) {
-//   alert ('Pozycja kursora na ekranie: ' + e.screenX + ', ' + e.screenY + ", czas " + e.timeStamp + ', zdarzenie ' + event.type);
-// }
-// window.addEventListener('dblclick', pozycjaKursora);
+window.addEventListener('click', () => {
+  const akapit = document.createElement('p');
+  const tekst = document.createTextNode('Lorem ipsum dolor sit amet...');
+  akapit.appendChild(tekst);
+  document.body.appendChild(akapit);
+})
 
 
