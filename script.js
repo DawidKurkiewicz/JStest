1187fccfdfce4a71e479a20647d63f0d2156304c
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-10-26 15:03:03 +0200

    exercise 5

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 
-
 document.querySelector('button').addEventListener('click', function() {
+function render(what, where) {
+    where.innerHTML = what;
+}
 var pole = document.getElementsByName('imie')[0];
 var komunikat = document.getElementById('info');
 
-if (pole.defaultValue === pole.value) {
-    komunikat.innerHTML = 'Wartość domyślna oraz wartość bieżąca są takie same:' + pole.defaultValue + ' oraz ' + pole.value + '';
-} else {
-    komunikat.innerHTML = 'Domyślna wartość to: ' + pole.defaultValue + ', zaś wartość bieżąca to: ' + pole.value + '';
-}
+    render( (pole.defaultValue === pole.value) ? (komunikat.innerHTML = 'Wartość domyślna oraz wartość bieżąca są takie same: ' + pole.defaultValue + ' oraz ' + pole.value + '') : (komunikat.innerHTML = 'Domyślna wartość to: ' + pole.defaultValue + ', zaś wartość bieżąca to: ' + pole.value + ''), komunikat );
+
 });
