07e2b523b7f930642f1338ce6094426faa876d15
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-10-26 12:05:57 +0200

    exercise 2 with even more js fun

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let index;
 for (index = 0; index < list.length; ++index) {
     list[index].style.fontSize = "60px";
     list[index].style.color = 'red'
-    list[index].addEventListener('click', color);
+    list[index].addEventListener('click', color1);
 
 
 
@@ -20,15 +20,26 @@ for (i = 0; i  < akapity.length; i++) {
     akapity[i].style.color = 'blue';
     akapity[i].style.fontSize = '40px';
     akapity[i].addEventListener('click', color);
+   
+
 
 }
 function color() {
   this.style.backgroundColor = 'green'
   this.style.fontSize = '80px'
+  this.style.color = 'black'
+
 }
-function pozycjaKursora(e) {
-  alert ('Pozycja kursora na ekranie: ' + e.screenX + ', ' + e.screenY + ", czas " + e.timeStamp + ', zdarzenie ' + event.type);
+function color1() {
+  this.style.backgroundColor = 'purple'
+  this.style.fontSize = '80px'
+  this.style.color = 'pink'
+  this.style.textAlign = 'center'
+
 }
-window.addEventListener('dblclick', pozycjaKursora);
+// function pozycjaKursora(e) {
+//   alert ('Pozycja kursora na ekranie: ' + e.screenX + ', ' + e.screenY + ", czas " + e.timeStamp + ', zdarzenie ' + event.type);
+// }
+// window.addEventListener('dblclick', pozycjaKursora);
 
 
