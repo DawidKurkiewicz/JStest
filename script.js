bf3947b75987f7f45d242a7bfc03975e37c28bc9
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-10-26 15:02:42 +0200

    exercise 4

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-document.getElementById('imie').value = 'Johnny Bravo';
+
+
+document.querySelector('button').addEventListener('click', function() {
+var pole = document.getElementsByName('imie')[0];
+var komunikat = document.getElementById('info');
+
+if (pole.defaultValue === pole.value) {
+    komunikat.innerHTML = 'Wartość domyślna oraz wartość bieżąca są takie same:' + pole.defaultValue + ' oraz ' + pole.value + '';
+} else {
+    komunikat.innerHTML = 'Domyślna wartość to: ' + pole.defaultValue + ', zaś wartość bieżąca to: ' + pole.value + '';
+}
+});
