1d874dd5ada28e0b2c04f8b7dd625b5e6ad79844
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-10-29 11:33:53 +0100

    other way to sort

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
-const kontynenty = ['europa', 'azja', 'antarktyda', 'afryka', 'australia', 'amerykapol', 'amerykapld'];
+// const kontynenty = ['europa', 'azja', 'antarktyda', 'afryka', 'australia', 'amerykapol', 'amerykapld'];
 
-kontynenty.sort();
-kontynenty.reverse();
+// kontynenty.sort();
+// kontynenty.reverse();
 
-console.log(kontynenty)
-
-
+// console.log(kontynenty)
 
 
-const liczby = [1, 12, 13, 0, 2, 20, 9, 3, 22, 10, -2, 11];
 
+
+var liczby = [1, 12, 13, 0, 2, 20, 9, 3, 22, 10, -2, 11];
 function sortNumber(a,b) {
     return a - b;
 }
-
-
 liczby.sort(sortNumber);
 console.log(liczby)
-
-
 liczby.reverse(sortNumber)
 console.log(liczby)
 
 
+function render (myArray) {
+    console.log(myArray.sort(function (x, y) {
+        return x - y }));
+    console.log(myArray.reverse(function (x, y) {
+        return x - y }));
 
+};
+var liczby = [1, 12, 13, 0, 2, 20, 9, 3, 22, 10, -2, 11];
+render(liczby)
