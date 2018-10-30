0ce14b7c017f12ca8b8a943b95b1e2fc677cb650
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-10-30 11:46:54 +0100

    exercise 12

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function numbers() {
-    var matma = Math.floor(Math.random() *10)
+    var matma = Math.min(2, 18, -10, 0, 11, 95, -29, 80, 22, 91, -91, 47, 81, 33, 59, -102, 21, 20, -14, 60);
+    matma = Math.abs(matma) 
+    matma = Math.sqrt(matma)
+    matma = Math.round(matma)
+    matma = Math.pow(matma, 2) * Math.PI
+    matma = Math.ceil(matma)
+
     console.log(matma)
 }
 numbers()
