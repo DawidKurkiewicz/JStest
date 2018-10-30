bd705b74bcfbb8981fea5d367a0f6d665b697940
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-10-30 11:54:39 +0100

    exercise 12 with render

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+
+function render(what, where) {
+    where.innerHTML = what;
+}
+
 function numbers() {
     var matma = Math.min(2, 18, -10, 0, 11, 95, -29, 80, 22, 91, -91, 47, 81, 33, 59, -102, 21, 20, -14, 60);
     matma = Math.abs(matma) 
@@ -5,7 +10,6 @@ function numbers() {
     matma = Math.round(matma)
     matma = Math.pow(matma, 2) * Math.PI
     matma = Math.ceil(matma)
-
-    console.log(matma)
+    return matma;
 }
-numbers()
+render(numbers(), document.body)
