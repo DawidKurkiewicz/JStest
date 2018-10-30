84891c8961cec1a030a06a83dd7bc3bf3f7c2b9a
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-10-30 12:18:01 +0100

    exercise 13 data

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 
-function render(what, where) {
-    where.innerHTML = what;
-}
-
-function numbers() {
-    var matma = Math.min(2, 18, -10, 0, 11, 95, -29, 80, 22, 91, -91, 47, 81, 33, 59, -102, 21, 20, -14, 60);
-    matma = Math.abs(matma) 
-    matma = Math.sqrt(matma)
-    matma = Math.round(matma)
-    matma = Math.pow(matma, 2) * Math.PI
-    matma = Math.ceil(matma)
-    return matma;
-}
-render(numbers(), document.body)
+var dzien = ['nd', 'pon', 'wt', 'sr', 'czw', 'pt', 'sob', ]
+var data =  new Date().getDay();
+data = dzien[data]
+console.log(data)
