6867bcd0b203da14668eab028a9ee3fe2fa10a74
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-10-30 10:38:54 +0100

    third way to do this

--- script.js
+++ script.js
@@ -26,25 +26,50 @@
 
 
 
+// const osoby = [
+//     {
+//         imie: 'Jan',
+//         nazwisko: 'Kowalski',
+//         wiek: 18,
+//     },
+//     {
+//         imie: 'Maria',
+//         nazwisko: 'Nowak',
+//         wiek: 21,
+//     }
+// ];
+
+// let tabela = '<table><tr><th>Imie i naziwsko</th><th>wiek</th></tr>';
+// for(cecha in osoby) {
+//     tabela += `<tr>`
+//     tabela +=  `<td>${osoby[cecha].imie} ${osoby[cecha].nazwisko}</td>`
+//     tabela += `<td>${osoby[cecha].wiek} lat</td>`
+//     tabela += `</tr>`
+// }
+// tabela += '</table>';
+// document.body.innerHTML = tabela
+
+
 const osoby = [
     {
         imie: 'Jan',
         nazwisko: 'Kowalski',
         wiek: 18,
+        imieNazwisko: function() { return this.imie + ' ' + this.nazwisko; }
+
     },
     {
         imie: 'Maria',
         nazwisko: 'Nowak',
         wiek: 21,
+        imieNazwisko: function() { return this.imie + ' ' + this.nazwisko; }
+
     }
 ];
 
 let tabela = '<table><tr><th>Imie i naziwsko</th><th>wiek</th></tr>';
 for(cecha in osoby) {
-    tabela += `<tr>`
-    tabela +=  `<td>${osoby[cecha].imie} ${osoby[cecha].nazwisko}</td>`
-    tabela += `<td>${osoby[cecha].wiek} lat</td>`
-    tabela += `</tr>`
+   tabela += `<tr><td>${osoby[cecha].imieNazwisko()}</td><td>${osoby[cecha].wiek}</td></tr>`
 }
 tabela += '</table>';
 document.body.innerHTML = tabela
